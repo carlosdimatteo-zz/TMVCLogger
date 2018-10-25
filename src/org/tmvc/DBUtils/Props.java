a61e3fd83c06e7ae0ccc9e3a538bfb8db8dc52c9
cdimatteo <carlosdima_97@hotmail.com>
2018-10-24 21:18:39 -0400

    Added propMap functions and added methods to log interface

--- src/org/tmvc/DBUtils/Props.java
+++ src/org/tmvc/DBUtils/Props.java
@@ -2,41 +2,79 @@ package org.tmvc.DBUtils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Properties;
+import java.util.Set;
 
 public class Props {
 
 
-    private static Props instance = null;
+    private static Props logInstance = null;
+    private static Props DBInstance = null;
     private static Properties properties = null;
+    private static HashMap<String,Boolean> DBPropMap= new HashMap<String,Boolean>();
+    private static HashMap<String,Boolean> logPropMap = new HashMap<String,Boolean>();
 
     protected Props(String path) throws IOException {
 
         properties = new Properties();
         FileInputStream is = new FileInputStream(path);
         properties.load(is);
+
     }
 
-    public static Props getInstance(String type) {//method  for singleton instance requesting
-        if(instance == null) {
-            try {
-        switch(type) {
-            case "logProp" :
-                instance = new Props("./../../../../log.properties");
-                break;
-            case "dbProp" :
-                instance = new Props("./../../../../database.properties");
-                break;
+    private void setPropMap(Properties properties, String path) {
+        HashMap<String, Boolean> propMap = new HashMap<String, Boolean>();
+        Set<String> propSet = properties.stringPropertyNames();
+        for (String propKey : propSet) {
+            if (path == "./../../../../log.properties") {
+                logPropMap.put(propKey, Boolean.parseBoolean(properties.getProperty(propKey)));
+            } else {
+
+                DBPropMap.put(propKey,Boolean.parseBoolean(properties.getProperty(propKey)));
+
+            }
         }
+    }
+
+    public static Props getLogPropsInstance() {//method  for singleton instance requesting
+        if(logInstance==null) {
+            try {
+                logInstance = new Props("./../../../../log.properties");
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            return logInstance;
+        }else{
+            return logInstance;
         }
-        return instance;
+    }
+
+    public static Props getDBPropsInstance() {//method  for singleton instance requesting
+        if(DBInstance==null) {
+            try {
+                DBInstance = new Props("./../../../../database.properties");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return DBInstance;
+        }else{
+            return DBInstance;
+        }
     }
 
     public  String getProp(String key) {
         return properties.getProperty(key);
+    }
+
+    public HashMap getDBPropMap(String propType){
+        return DBPropMap;
+
+    }
+
+    public HashMap getLogPropMap(String propType){
+        return logPropMap;
+
     }
 
 }
